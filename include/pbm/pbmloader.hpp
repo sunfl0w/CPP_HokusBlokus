fd7db28a0f2769e47d592400d65820267724338b
sunfl0w <lucaanthonyschwarz@googlemail.com>
2020-09-07 23:10:10 +0200

    Still working on the pbm loader.

--- include/pbm/pbmloader.hpp
+++ include/pbm/pbmloader.hpp
@@ -3,6 +3,7 @@
 #include "pbmimage.hpp"
 #include <string>
 #include <fstream>
+#include <iostream>
 
 namespace HokusBlokus::PBM {
     class PBMLoader {
--- src/pbm/pbmloader.cpp
+++ src/pbm/pbmloader.cpp
@@ -2,19 +2,31 @@
 
 namespace HokusBlokus::PBM {
     PBMImage PBMLoader::ReadPBMToVector(std::string path) {
-        unsigned int width, height;
-        //Rad bitmmap metadata
-
+        unsigned int width = 0;
+        unsigned int height = 0;
+        //Read bitmmap metadata. We will only load the P4 format
+        std::ifstream inputStream(path, std::ios::binary);
+        std::string line;
+        int lineCount = 0;
+        while (std::getline(inputStream, line) && width == height == 0) {
+            if (lineCount == 0 && line != "P4") {
+                std::cout << "Unable to read pbm file. Expected type P4.\n";
+                std::exit(1);
+            } else if(line[0] != '#') {
+                line.find(' ');
+            }
+            lineCount++;
+        }
 
         //Read bitmap data
-        std::ifstream inputStream(path, std::ios::binary);
-        boost::dynamic_bitset<> data = boost::dynamic_bitset<>(width * height); 
+        std::ifstream inputStreamBinary(path, std::ios::binary);
+        boost::dynamic_bitset<> data = boost::dynamic_bitset<>(width * height);
 
         char byte;
         int bitCount = 0;
-        while(inputStream.get(byte)) {
-            for(int i = 7; i >= 0; i--) {
-                if((byte >> i) & 1) {
+        while (inputStreamBinary.get(byte) && bitCount < width * height) {
+            for (int i = 7; i >= 0; i--) {
+                if ((byte >> i) & 1) {
                     data[bitCount % width, bitCount / width];
                 }
                 bitCount++;
@@ -23,4 +35,4 @@ namespace HokusBlokus::PBM {
 
         return PBMImage(width, height, data);
     }
-}
+}  // namespace HokusBlokus::PBM
